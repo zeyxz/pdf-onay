38ab5b8ad6047991e53ff43b2685c45960cd3335
zere <zirveilaclamax@gmail.com>
2026-05-18 14:29:05 +0300

    debug

--- backend/server.js
+++ backend/server.js
@@ -217,10 +217,15 @@ app.post("/send-mail", async (req, res) => {
            host: "smtp.gmail.com",
     port: 587,
     secure: false,
+      requireTLS: true,
+      connectionTimeout:30000,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
-            }
+            },
+              tls: {
+        rejectUnauthorized: false
+    }
         });
 
         console.log("MAIL GÖNDERİLİYOR");
