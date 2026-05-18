911178b5fb20e190c0f691cdbc47514c882f7fc1
zere <zirveilaclamax@gmail.com>
2026-05-18 13:55:28 +0300

    smtp fix

--- backend/server.js
+++ backend/server.js
@@ -214,7 +214,9 @@ app.post("/send-mail", async (req, res) => {
 
         // mail gönder
         let transporter = nodemailer.createTransport({
-            service: "gmail",
+           host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
